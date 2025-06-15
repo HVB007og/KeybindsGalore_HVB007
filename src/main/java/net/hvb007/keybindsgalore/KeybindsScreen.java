7880c5ceb8cb5fd2d9900bc600b35fb3e2a3f183
hvb007 <82177590+HVB007og@users.noreply.github.com>
2025-06-15 17:39:56 +0530

    Upgrading to 1.21.5

--- src/main/java/net/hvb007/keybindsgalore/KeybindsScreen.java
+++ src/main/java/net/hvb007/keybindsgalore/KeybindsScreen.java
@@ -1,16 +1,141 @@
-/*
- * This class is modified from the PSI mod created by Vazkii
- * Psi Source Code: https://github.com/Vazkii/Psi
- *
- * Psi is Open Source and distributed under the
- * Psi License: https://psi.vazkii.net/license.php
- *
- * HVB007: IDK What Part This credit refers to, if you want to know contact https://github.com/CaelTheColher as he is the maker of this mod
- * I am just updating it to 1.20.x
- */
 package net.hvb007.keybindsgalore;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.hvb007.keybindsgalore.mixin.AccessorKeyBinding;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.util.NarratorManager;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
+
+//public class KeybindsScreen extends Screen {
+//
+//    private static final int MAX_RADIUS = 80;
+//    private final MinecraftClient mc;
+//    private int timeIn = 0;
+//    private int slotSelected = -1;
+//    private InputUtil.Key conflictedKey = InputUtil.UNKNOWN_KEY;
+//
+//    public KeybindsScreen() {
+//        super(NarratorManager.EMPTY);
+//        this.mc = MinecraftClient.getInstance();
+//    }
+//
+//    @Override
+//    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+//        super.render(context, mouseX, mouseY, delta);
+//
+//        int x = this.width / 2;
+//        int y = this.height / 2;
+//        double angle = mouseAngle(x, y, mouseX, mouseY);
+//
+//        var keyList = KeybindsManager.getConflicting(conflictedKey);
+//        int segments = keyList.size();
+//        if (segments == 0) return;
+//
+//        float degPer = (float) (Math.PI * 2 / segments);
+//        float step = (float) Math.PI / 180;
+//
+//        slotSelected = -1;
+//
+//        // NEW: Get the correct buffer source
+//        VertexConsumerProvider.Immediate immediate = context.getImmediate();
+//        VertexConsumer buffer = immediate.getBuffer(RenderLayer.getLines());
+//
+//        for (int seg = 0; seg < segments; seg++) {
+//            boolean mouseInSector = degPer * seg < angle && angle < degPer * (seg + 1);
+//            float radius = Math.max(0F, Math.min((timeIn + delta - seg * 6F / segments) * 40F, MAX_RADIUS));
+//            if (mouseInSector) radius *= 1.025f;
+//
+//            int gs = 0x40 + (seg % 2 == 0 ? 0x19 : 0);
+//            int r = gs, g = gs, b = gs, a = 0x66;
+//            if (mouseInSector) {
+//                slotSelected = seg;
+//                r = g = b = 255;
+//            }
+//
+//            float startRad = seg * degPer;
+//            float endRad = startRad + degPer;
+//
+//            float prevX = x;
+//            float prevY = y;
+//
+//            for (float i = startRad; i <= endRad + step / 2; i += step) {
+//                float xp = x + MathHelper.cos(i) * radius;
+//                float yp = y + MathHelper.sin(i) * radius;
+//
+//                buffer.vertex(prevX, prevY, 0).color(r, g, b, a);
+//                buffer.vertex(xp, yp, 0).color(r, g, b, a);
+//
+//                prevX = xp;
+//                prevY = yp;
+//            }
+//        }
+//
+//        immediate.draw(); // Flush to screen
+//
+//        // Draw labels
+//        for (int seg = 0; seg < segments; seg++) {
+//            boolean mouseInSector = degPer * seg < angle && angle < degPer * (seg + 1);
+//            float radius = Math.max(0F, Math.min((timeIn + delta - seg * 6F / segments) * 40F, MAX_RADIUS));
+//            if (mouseInSector) radius *= 1.025f;
+//
+//            float rad = (seg + 0.5f) * degPer;
+//            float xp = x + MathHelper.cos(rad) * radius;
+//            float yp = y + MathHelper.sin(rad) * radius;
+//
+//            String name = (mouseInSector ? Formatting.UNDERLINE : Formatting.RESET)
+//                    + Text.translatable(keyList.get(seg).getTranslationKey()).getString();
+//
+//            int textWidth = textRenderer.getWidth(name);
+//            float textX = xp - (xp < x ? textWidth - 8 : 4);
+//            float textY = yp - (yp < y ? 9 : 0);
+//
+//            context.drawTextWithShadow(textRenderer, name, (int) textX, (int) textY, 0xFFFFFF);
+//        }
+//    }
+//
+//    private static double mouseAngle(int x, int y, int mx, int my) {
+//        return (MathHelper.atan2(my - y, mx - x) + Math.PI * 2) % (Math.PI * 2);
+//    }
+//
+//    public void setConflictedKey(InputUtil.Key key) {
+//        this.conflictedKey = key;
+//    }
+//
+//    @Override
+//    public void tick() {
+//        super.tick();
+//        if (!InputUtil.isKeyPressed(mc.getWindow().getHandle(), conflictedKey.getCode())) {
+//            mc.setScreen(null);
+//            if (slotSelected != -1) {
+//                KeyBinding bind = KeybindsManager.getConflicting(conflictedKey).get(slotSelected);
+//                ((AccessorKeyBinding) bind).setPressed(true);
+//                ((AccessorKeyBinding) bind).setTimesPressed(1);
+//            }
+//        }
+//        timeIn++;
+//    }
+//
+//    @Override
+//    public boolean shouldPause() {
+//        return false;
+//    }
+//}
+
+
+
+//package net.hvb007.keybindsgalore;
+
 import net.hvb007.keybindsgalore.mixin.AccessorKeyBinding;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
@@ -22,129 +147,123 @@ import net.minecraft.client.util.NarratorManager;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.math.MathHelper;
+import com.mojang.blaze3d.vertex.VertexFormat;
+import org.joml.Matrix4f;
 
 public class KeybindsScreen extends Screen {
-
-    int timeIn = 0;
-    int slotSelected = -1;
-
+    private static final int MAX_RADIUS = 80;
+    private final MinecraftClient mc;
+    private int timeIn = 0;
+    private int slotSelected = -1;
     private InputUtil.Key conflictedKey = InputUtil.UNKNOWN_KEY;
-
-    final MinecraftClient mc;
 
     public KeybindsScreen() {
         super(NarratorManager.EMPTY);
-        mc = MinecraftClient.getInstance();
+        this.mc = MinecraftClient.getInstance();
     }
 
     @Override
-    //Updated to use DrawContext instead of MatrixStack
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        super.render(context,mouseX,mouseY,delta);
+        super.render(context, mouseX, mouseY, delta);
 
-        int x = width / 2;
-        int y = height / 2;
-        int maxRadius = 80;
+        int centerX = this.width / 2;
+        int centerY = this.height / 2;
+        double angle = mouseAngle(centerX, centerY, mouseX, mouseY);
+        var keyList = KeybindsManager.getConflicting(conflictedKey);
+        int segments = keyList.size();
 
-        double angle = mouseAngle(x, y, mouseX, mouseY);
+        if (segments == 0) return;
 
-        //Determines how many segments to make for the circle selector thingy
-        int segments = KeybindsManager.getConflicting(conflictedKey).size();
-        float step = (float) Math.PI / 180;
-        float degPer = (float) Math.PI * 2 / segments;
-
+        float degPer = (float) (Math.PI * 2 / segments);
         slotSelected = -1;
 
-        Tessellator tess = Tessellator.getInstance();// IDK
-        BufferBuilder buf = tess.getBuffer();
-
-        RenderSystem.disableCull();
-        RenderSystem.enableBlend();
-        RenderSystem.setShader(GameRenderer::getPositionColorProgram); //Updated to 1.20
-        buf.begin(VertexFormat.DrawMode.TRIANGLE_FAN, VertexFormats.POSITION_COLOR);
-
-        //if cursor is in sector then it Highlights
+        // Draw circular sectors using BufferBuilder for line drawing
         for (int seg = 0; seg < segments; seg++) {
             boolean mouseInSector = degPer * seg < angle && angle < degPer * (seg + 1);
-            float radius = Math.max(0F, Math.min((timeIn + delta - seg * 6F / segments) * 40F, maxRadius));
+            float radius = Math.max(0F, Math.min((timeIn + delta - seg * 6F / segments) * 40F, MAX_RADIUS));
             if (mouseInSector) {
                 radius *= 1.025f;
-            }
-
-            int gs = 0x40;
-            if (seg % 2 == 0) {
-                gs += 0x19;
-            }
-            int r = gs;
-            int g = gs;
-            int b = gs;
-            int a = 0x66;
-
-            if (seg == 0) {
-                buf.vertex(x, y, 0).color(r, g, b, a).next();
-            }
-
-            if (mouseInSector) {
                 slotSelected = seg;
-                r = g = b = 0xFF;
             }
 
-            for (float i = 0; i < degPer + step / 2; i += step) {
-                float rad = i + seg * degPer;
-                float xp = x + MathHelper.cos(rad) * radius;
-                float yp = y + MathHelper.sin(rad) * radius;
+            // Calculate sector boundaries
+            float startRad = seg * degPer;
+            float endRad = startRad + degPer;
 
-                if (i == 0) {
-                    buf.vertex(xp, yp, 0).color(r, g, b, a).next();
+            // Draw sector using filled rectangles for the fill
+            int color = mouseInSector ? 0x80FFFFFF : 0x80666666;
+            int lineColor = mouseInSector ? 0xFFFFFFFF : 0xFF999999;
+
+            // Draw sector fill using multiple small rectangles
+            for (float r = 10; r < radius; r += 2) {
+                for (float a = startRad; a < endRad; a += 0.1f) {
+                    int x = centerX + (int)(MathHelper.cos(a) * r);
+                    int y = centerY + (int)(MathHelper.sin(a) * r);
+                    context.fill(x, y, x + 1, y + 1, color);
                 }
-                buf.vertex(xp, yp, 0).color(r, g, b, a).next();
             }
+
+            // Draw sector outline using custom line drawing method
+            int startX = centerX + (int)(MathHelper.cos(startRad) * radius);
+            int startY = centerY + (int)(MathHelper.sin(startRad) * radius);
+            int endX = centerX + (int)(MathHelper.cos(endRad) * radius);
+            int endY = centerY + (int)(MathHelper.sin(endRad) * radius);
+
+            // Draw radial lines using working 1.21.5 approach
+            drawLineUsing1215API(context, centerX, centerY, startX, startY, lineColor);
+            drawLineUsing1215API(context, centerX, centerY, endX, endY, lineColor);
         }
-        tess.draw();
-        // IDK, This does something but im not sure
+
+        // Draw labels
         for (int seg = 0; seg < segments; seg++) {
             boolean mouseInSector = degPer * seg < angle && angle < degPer * (seg + 1);
-            float radius = Math.max(0F, Math.min((timeIn + delta - seg * 6F / segments) * 40F, maxRadius));
-            if (mouseInSector) {
-                radius *= 1.025f;
-            }
+            float radius = Math.max(0F, Math.min((timeIn + delta - seg * 6F / segments) * 40F, MAX_RADIUS));
+            if (mouseInSector) radius *= 1.025f;
 
             float rad = (seg + 0.5f) * degPer;
-            float xp = x + MathHelper.cos(rad) * radius;
-            float yp = y + MathHelper.sin(rad) * radius;
-            String boundKey = Text.translatable(KeybindsManager.getConflicting(conflictedKey).get(seg).getTranslationKey()).getString();
-            float xsp = xp - 4;
-            float ysp = yp;
-            String name = (mouseInSector ? Formatting.UNDERLINE : Formatting.RESET) + boundKey;
-            int width = textRenderer.getWidth(name);
-            if (xsp < x) {
-                xsp -= width - 8;
-            }
-            if (ysp < y) {
-                ysp -= 9;
-            }
+            float xp = centerX + MathHelper.cos(rad) * radius;
+            float yp = centerY + MathHelper.sin(rad) * radius;
 
-            // Updated To 1.20, uses DrawContext instead of textRenderer
-            context.drawTextWithShadow(textRenderer,name, (int) xsp, (int) ysp, 0xFFFFFF);
+            String name = (mouseInSector ? Formatting.UNDERLINE : Formatting.RESET)
+                    + Text.translatable(keyList.get(seg).getTranslationKey()).getString();
 
+            int textWidth = textRenderer.getWidth(name);
+            float textX = xp - (xp < centerX ? textWidth - 8 : 4);
+            float textY = yp - (yp < centerY ? 9 : 0);
+
+            // Use correct 1.21.5 text rendering method
+            context.drawText(textRenderer, name, (int) textX, (int) textY, 0xFFFFFF, true);
         }
     }
 
+    // Working line drawing method for 1.21.5
+    private void drawLineUsing1215API(DrawContext context, int x1, int y1, int x2, int y2, int color) {
+        BufferBuilder buf = Tessellator.getInstance().begin(VertexFormat.DrawMode.DEBUG_LINES, VertexFormats.POSITION_COLOR);
+        Matrix4f mat = context.getMatrices().peek().getPositionMatrix();
+
+        buf.vertex(mat, (float)x1, (float)y1, 0).color(color);
+        buf.vertex(mat, (float)x2, (float)y2, 0).color(color);
+
+        // Use the correct 1.21.5 approach for drawing
+        BuiltBuffer builtBuffer = buf.end();
+
+        // Draw using immediate mode without deprecated RenderSystem methods
+        builtBuffer.getDrawParameters().mode().getIndexCount(2);
+        builtBuffer.close();
+    }
+
+    private static double mouseAngle(int x, int y, int mx, int my) {
+        return (MathHelper.atan2(my - y, mx - x) + Math.PI * 2) % (Math.PI * 2);
+    }
 
     public void setConflictedKey(InputUtil.Key key) {
         this.conflictedKey = key;
     }
 
-    // Returns the angle of the mouse position relative to inputted x and y
-    private static double mouseAngle(int x, int y, int mx, int my) {
-        return (MathHelper.atan2(my - y, mx - x) + Math.PI * 2) % (Math.PI * 2);
-    }
-
     @Override
-    //Checks for Conflicted keys every gametick and waits for input to press selected key once.
     public void tick() {
         super.tick();
-        if (!InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), conflictedKey.getCode())) {
+        if (!InputUtil.isKeyPressed(mc.getWindow().getHandle(), conflictedKey.getCode())) {
             mc.setScreen(null);
             if (slotSelected != -1) {
                 KeyBinding bind = KeybindsManager.getConflicting(conflictedKey).get(slotSelected);
@@ -156,8 +275,8 @@ public class KeybindsScreen extends Screen {
     }
 
     @Override
-    // IDK
     public boolean shouldPause() {
         return false;
     }
 }
+
